4eaa981c7c0947b762df173570c3a2d3e209ca94
ingdaeho <62834439+ingdaeho@users.noreply.github.com>
2021-05-11 18:14:31 +0900

    Add: style reset

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,34 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./app/App";
-import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import App from "@app/App";
+import createStore from "@app/store";
+
+const store = createStore();
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {
+  margin: 0;
+  font-family: "AppleSDGothicNeo", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+    "Helvetica Neue", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 15px;
+  box-sizing: border-box;
+}
+
+* {
+  box-sizing: inherit;
+}
+`;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <GlobalStyle />
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
