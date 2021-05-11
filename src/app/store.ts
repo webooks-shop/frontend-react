c0c574e679f02c49a44ecaa74f2c1c33a4fa654e
ingdaeho <62834439+ingdaeho@users.noreply.github.com>
2021-05-11 18:03:44 +0900

    Add: 초기세팅

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,23 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import { all } from "redux-saga/effects";
+import rootReducer from "@app/rootReducer";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+const sagaMiddleware = createSagaMiddleware();
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+function* rootSaga() {
+  yield all([]);
+}
+
+const createStore = () => {
+  const store = configureStore({
+    reducer: rootReducer,
+    devTools: true,
+    middleware: [sagaMiddleware],
+  });
+
+  sagaMiddleware.run(rootSaga);
+  return store;
+};
+
+export default createStore;
